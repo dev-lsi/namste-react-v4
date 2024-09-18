9e871555c5b5beaa9d48f038b43281bded41cfbc
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-09-18 14:34:20 +0300

    12

--- src/utils/checkIsValidUserLocation.js
+++ src/utils/checkIsValidUserLocation.js
@@ -1,11 +1,12 @@
 import { url_base } from "./constants";
 
 export async function checkIsValidUserLocation(coords) {
+  alert("innnnnn")
   alert("in Validity:"+ "LAT"+coords.lat +"LNG:"+coords.lng)
   const url = url_base + "lat=" + coords.lat + "&lng=" + coords.lng;
   const response = await fetch(url);
-  alert(Object.keys(response));
+  alert("response keys:"+Object.keys(response));
   const responseData = await response.json();
-
+  alert("responseData keys:"+Object.keys(responseData));
   return responseData.data.success.cards ? true : false;
 }
