a4bd31d402de1357ba4cf163fee815d1d457a78f
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-10-02 17:30:45 +0300

    sotial icons in mobile menu fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,20 @@ const Footer=()=>{
 
     const changeTheme=()=>{
         
-        const currBackground = document.documentElement.style.getPropertyValue('--bg-dark-theme');
-        const currTextColor = document.documentElement.style.getPropertyValue('--color-dark-theme');
+        const currBackground = document.documentElement.style.getPropertyValue('--bg');
+        const currTextColor = document.documentElement.style.getPropertyValue('--color');
       
         if(currBackground =='rgb(34,34,34)'){ //sets light
-            document.documentElement.style.setProperty('--bg-dark-theme', 'lightgrey');
-            document.documentElement.style.setProperty('--color-dark-theme', 'rgb(34,34,34)');
+            document.documentElement.style.setProperty('--bg', 'grey');
+            document.documentElement.style.setProperty('--color', 'rgb(34,34,34)');
         }else{
-            document.documentElement.style.setProperty('--bg-dark-theme', 'rgb(34,34,34)');
-            document.documentElement.style.setProperty('--color-dark-theme', 'antiquewhite');
+            document.documentElement.style.setProperty('--bg', 'rgb(34,34,34)');
+            document.documentElement.style.setProperty('--color', 'antiquewhite');
         }
         
     }
     return (
         <div className={s["footer"]}>
-            <h4>Footer.....</h4>
             <button onClick={changeTheme}>Theme</button>
         </div>
     );
