dd9be00911d3c47938aed82a4415f3c75a0a4568
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-10-09 16:22:36 +0300

    swiggy changed data objects. New paths added

--- src/utils/getMenuData.js
+++ src/utils/getMenuData.js
@@ -2,8 +2,8 @@ export async function getMenuData(url,setData){
     
     const response = await fetch(url);
     const rawMenuData = await response.json();
-    
-    const list = rawMenuData.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards;
+    console.log(rawMenuData)
+    const list = rawMenuData?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards||rawMenuData?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
 
     const filtered=list.filter(x=>x.card.card["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
     
--- src/utils/getRestaurants.js
+++ src/utils/getRestaurants.js
@@ -2,11 +2,11 @@ export async function getRestaurants(url,setResCtx) {
 
     const response = await fetch(url);
     const responseData = await response.json();
-    console.log(responseData)
+    //console.log(responseData)
     // const restaurants_old =
     //   responseData.data.success.cards[1].card.card.gridElements.infoWithStyle.restaurants;
     
-    const restaurants = (responseData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    const restaurants = (responseData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)||(responseData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     setResCtx({ restaurants: [...restaurants] });
   }
